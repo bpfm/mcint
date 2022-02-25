f7137fe900787b76776fc4c8a11fae80bd28937c
Ben Morton <morton@roe.ac.uk>
2022-02-25 15:20:10 +0000

    generate nRand random int

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include "func1D.h"
+
 int main(){
+
+  srand(123456);      // initialise random seed
+
+  const int nRand = 10;          // number of random integers to draw
+
+  int iRand;                     // random integer
+
+  for(int i=0; i<nRand; i++){
+    iRand = rand() % 100;
+    printf("%i\t%i\n", iRand,i);
+  }
 
   return 0;
 }
