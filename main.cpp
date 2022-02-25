77981296b0ca4f5b7d5d3393729302a8368542f8
Ben Morton <morton@roe.ac.uk>
2022-02-25 15:44:51 +0000

    generate nRand random floats between lowerLimit and upperLimit

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@ int main(){
 
   srand(123456);      // initialise random seed
 
-  const int nRand = 10;          // number of random integers to draw
+  const int nRand = 10;          // number of random integers to draws
+  const float lowerLimit = 0.0;  // lower limit
+  const float upperLimit = 1.0;  // upper integral
 
-  int iRand;                     // random integer
+  float fRand;                   // random integer
 
   for(int i=0; i<nRand; i++){
-    iRand = rand() % 100;
-    printf("%i\t%i\n", iRand,i);
+    fRand = lowerLimit + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/(upperLimit - lowerLimit)));
+    printf("%i\t%f\n",i ,fRand);
   }
 
   return 0;
