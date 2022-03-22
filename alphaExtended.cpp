09c4417297a5c4528514b478cce59f532f9957a2
Ben Morton <morton@roe.ac.uk>
2022-03-22 13:53:19 +0000

    fixed missing declaration

--- alphaExtended.cpp
+++ alphaExtended.cpp
@@ -7,7 +7,7 @@ float alphaExtended(float xx, float yy, float zz){
   float yp = 0.0;
   float zp = -0.1;
 
-  alpha = alphaAnalytic(xp - xx, yp - yy, zp - zz)*perturberExtended(xx, yy, zz);
+  float alpha = alphaAnalytic(xp - xx, yp - yy, zp - zz)*perturberExtended(xx, yy, zz);
 
   return alpha;
 
