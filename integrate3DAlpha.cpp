3b87a4dd23692d9008c9a795e79759c497dd791a
Ben Morton <morton@roe.ac.uk>
2022-03-24 17:40:57 +0000

    specialised extended alpha and integration functions

---
+++ integrate3DAlpha.cpp
@@ -0,0 +1,25 @@
+// integrate3DAlpha.cpp
+#include <stdio.h>
+#include <stdlib.h>     /* srand, rand */
+#include <omp.h>
+
+int integrate3DAlpha(float xp, float yp, float zp,int nRand, float lowerLimitX, float upperLimitX, float lowerLimitY, float upperLimitY, float lowerLimitZ, float upperLimitZ, float lowerLimitRho, float upperLimitRho){
+  float fRandX,fRandY,fRandZ,fRandRho;          // random samples
+  float fourVol = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ)*(upperLimitRho - lowerLimitRho);
+  int nAccept = 0;
+
+  srand(123456);      // initialise random seed
+
+  #pragma omp parallel for
+  for(int i=0; i<nRand; i++){
+    fRandX   = lowerLimitX   + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/(upperLimitX   - lowerLimitX)));
+    fRandY   = lowerLimitY   + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/(upperLimitY   - lowerLimitY)));
+    fRandZ   = lowerLimitZ   + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/(upperLimitZ   - lowerLimitZ)));
+    fRandRho = lowerLimitRho + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/(upperLimitRho - lowerLimitRho)));
+    if(fRandRho <= alphaExtended(xp,yp,zp,fRandX,fRandY,fRandZ)){
+      nAccept++;
+    }
+    if(i % (nRand/100) == 0){printf("%i\t%f\t%f\n",i,fourVol,fourVol*float(nAccept)/float(i));}
+  }
+  return nAccept;
+}
