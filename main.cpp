d9e462cb1b623a4f699d7a3396d75754bd58d324
Ben Morton <morton@roe.ac.uk>
2022-04-04 13:27:06 +0100

    general comments

--- main.cpp
+++ main.cpp
@@ -13,16 +13,18 @@
 
 int main(int argc, char *argv[]){
 
+  /* setup limits of integration region */
   const int nRand = 5e7;        // number of random integers to draws
-  const float lowerLimitX = -1.0;  // lower limit
-  const float upperLimitX = 1.0;  // upper limt
-  const float lowerLimitY = -1.0;  // lower limit
-  const float upperLimitY = 1.0;  // upper limit
-  const float lowerLimitZ = -1.0;  // lower limit
-  const float upperLimitZ = 3.0;  // upper limit
-  const float lowerLimitRho = 0.0;  // lower limit
-  const float upperLimitRho = 1000.0;  // upper limit
+  const float lowerLimitX = -1.0;  // lower limit x
+  const float upperLimitX = 1.0;  // upper limt x
+  const float lowerLimitY = -1.0;  // lower limit y
+  const float upperLimitY = 1.0;  // upper limit y
+  const float lowerLimitZ = -1.0;  // lower limit z
+  const float upperLimitZ = 3.0;  // upper limit z
+  const float lowerLimitRho = 0.0;  // lower limit 4th dim
+  const float upperLimitRho = 1000.0;  // upper limit 4th dim
 
+  /* setup 1D, 2D, and 3D volumes for integration regions */
   float area    = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY);
   float vol     = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ);
   float fourVol = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ)*(upperLimitRho - lowerLimitRho);
@@ -48,14 +50,15 @@ int main(int argc, char *argv[]){
   // printf("*********************************\n");
   // printf("%f\n", float((nAccept)/float(nRand))*fourVol);
 
-  int nPoints = 99;
-  float xp = 0.0, yp = 0.0,zp;
-  FILE * pFile;
+  /* setup alpha integration for multiple z-position samples to form profile */
+  int nPoints = 99;                                                       // number of z-samples
+  float xp = 0.0, yp = 0.0,zp, lowerLimitZp = -0.5, upperLimitZp = 1.5;   // x,y position of profile, lower and upper limits of profile sample range
+  FILE * pFile;                                                           // output file
   pFile = fopen("zAlpha.txt","w");
 
+  /* call integration at all z sample position */
   for(int i=0; i<nPoints; i++){
-    // zp = (upperLimitZ - lowerLimitZ)*float(i)/float(nPoints) + lowerLimitZ;
-    zp = (1.5 - (-0.5))*float(i)/float(nPoints) + (-0.5);
+    zp = (upperLimitZp - (lowerLimitZp))*float(i)/float(nPoints) + (lowerLimitZp); // iterate sample position
     nAccept = integrate3DAlpha(xp,yp,zp,nRand,lowerLimitX,upperLimitX,lowerLimitY,upperLimitY,lowerLimitZ,upperLimitZ,lowerLimitRho,upperLimitRho);
     printf("%f\t%i\t%f\t%f\n", zp, nRand, float((nAccept)/float(nRand))*fourVol, alphaAnalytic(xp,yp,zp));
     fprintf(pFile, "%f\t%f\t%f\t%f\n", zp, float((nAccept)/float(nRand))*fourVol, alphaAnalytic(xp,yp,zp),perturberExtended(xp,yp,zp));
