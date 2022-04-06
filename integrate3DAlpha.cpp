d5bf2384f333889e3dfc2e0e2c20ce3066e98509
Ben Morton <morton@roe.ac.uk>
2022-04-06 10:52:25 +0100

    further commenting

--- integrate3DAlpha.cpp
+++ integrate3DAlpha.cpp
@@ -7,8 +7,8 @@
 
 int integrate3DAlpha(float xp, float yp, float zp, int nRand, float lowerLimitX, float upperLimitX, float lowerLimitY, float upperLimitY, float lowerLimitZ, float upperLimitZ, float lowerLimitRho, float upperLimitRho){
   float fRandX,fRandY,fRandZ,fRandRho;          // random samples
-  float fourVol = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ)*(upperLimitRho - lowerLimitRho);
-  int nAccept = 0;
+  float fourVol = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ)*(upperLimitRho - lowerLimitRho); // 4D volume
+  int nAccept = 0;  // number of accepted random samples
 
   srand(123456);      // initialise random seed
 
--- main.cpp
+++ main.cpp
@@ -16,21 +16,21 @@
 int main(int argc, char *argv[]){
 
   /* setup limits of integration region */
-  const int nRand = 1e7;        // number of random integers to draws
-  const float lowerLimitX = 0;  // lower limit x
-  const float upperLimitX = 1.0;  // upper limt x
-  const float lowerLimitY = 0;  // lower limit y
-  const float upperLimitY = 1.0;  // upper limit y
-  const float lowerLimitZ = 0;  // lower limit z
-  const float upperLimitZ = 1.0;  // upper limit z
+  const int nRand = 1e7;            // number of random integers to draws
+  const float lowerLimitX = 0;      // lower limit x
+  const float upperLimitX = 1.0;    // upper limt x
+  const float lowerLimitY = 0;      // lower limit y
+  const float upperLimitY = 1.0;    // upper limit y
+  const float lowerLimitZ = 0;      // lower limit z
+  const float upperLimitZ = 1.0;    // upper limit z
   const float lowerLimitRho = 0.0;  // lower limit 4th dim
-  const float upperLimitRho = 10.0;  // upper limit 4th dim
+  const float upperLimitRho = 10.0; // upper limit 4th dim
 
   /* setup 1D, 2D, and 3D volumes for integration regions */
-  float area    = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY);
-  float vol     = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ);
-  float fourVol = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ)*(upperLimitRho - lowerLimitRho);
-  int nAccept;
+  float area    = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY);                                                              // 2D volume
+  float vol     = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ);                                  // 3D volume       
+  float fourVol = (upperLimitX - lowerLimitX)*(upperLimitY - lowerLimitY)*(upperLimitZ - lowerLimitZ)*(upperLimitRho - lowerLimitRho);  // 4D volume
+  int nAccept;                                                                                                                          // number of accepted random samples
 
   // /* 1D test integral */
   // nAccept = integrate1D(nRand,lowerLimitX,upperLimitX,lowerLimitY,upperLimitY,func1D);
@@ -48,14 +48,14 @@ int main(int argc, char *argv[]){
   // printf("%f\n", float((nAccept)/float(nRand))*fourVol);
 
   /* setup alpha integration for multiple z-position samples to form profile */
-  int nPoints = 99;                                                       // number of z-samples
+  int nPoints = 99;                                                        // number of z-samples
   float xp = 0.0, yp = 0.0, zp, lowerLimitZp = -0.5, upperLimitZp = 1.5;   // x,y position of profile, lower and upper limits of profile sample range
   FILE * pFile;                                                            // output file
   pFile = fopen("zAlpha.txt","w");
   
   /* call integration at all z sample position */
   for(int i=0; i<nPoints; i++){
-    zp = (upperLimitZp - (lowerLimitZp))*float(i)/float(nPoints) + (lowerLimitZp); // iterate sample position
+    zp = (upperLimitZp - (lowerLimitZp))*float(i)/float(nPoints) + (lowerLimitZp);
     nAccept = integrate3DAlpha(xp,yp,zp,nRand,lowerLimitX,upperLimitX,lowerLimitY,upperLimitY,lowerLimitZ,upperLimitZ,lowerLimitRho,upperLimitRho);
     printf("%f\t%i\t%f\t%f\n", zp, nAccept, fourVol*float(nAccept)/float(nRand), alphaAnalytic(xp,yp,zp));
     fprintf(pFile, "%f\t%f\t%f\t%f\n", zp, fourVol*float(nAccept)/float(nRand), alphaAnalytic(xp,yp,zp),perturberExtended(xp,yp,zp));
@@ -69,7 +69,7 @@ int main(int argc, char *argv[]){
 
   // /* call integration at all z sample position */
   // for(int i=0; i<nPoints; i++){
-  //   zp = (upperLimitZp - (lowerLimitZp))*float(i)/float(nPoints) + (lowerLimitZp); // iterate sample position
+  //   zp = (upperLimitZp - (lowerLimitZp))*float(i)/float(nPoints) + (lowerLimitZp);
   //   nAccept = integrate3DTest(xp,yp,zp,nRand,lowerLimitX,upperLimitX,lowerLimitY,upperLimitY,lowerLimitZ,upperLimitZ,lowerLimitRho,upperLimitRho);
   //   printf("%f\t%i\t%f\n", zp, nAccept, fourVol*float(nAccept)/float(nRand));
   //   fprintf(pFile, "%f\t%f\n", zp, fourVol*float(nAccept)/float(nRand));
