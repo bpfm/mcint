9c8ae9b46b8f2c760fbc62a0c0aa52345ee42653
Ben Morton <morton@roe.ac.uk>
2022-11-10 15:55:19 +0000

    new mean value method

---
+++ integrateMean1D.cpp
@@ -0,0 +1,21 @@
+// integrateMean1D.cpp
+#include <stdio.h>
+#include <stdlib.h>     /* srand, rand */
+#include <omp.h>
+
+float integrateMean1D(int nRand, float lowerLimitX, float upperLimitX, float (*func1D)(float)){
+  float fRandX,fRandY,funcTot = 0.0,funcMean;           // random samples
+  float length = (upperLimitX - lowerLimitX);
+
+  srand(123456);      // initialise random seed
+
+  #pragma omp parallel for
+  for(int i=0; i<nRand; i++){
+    fRandX = lowerLimitX + static_cast <float> (rand()) / (static_cast <float> (float(RAND_MAX)/(upperLimitX - lowerLimitX)));
+    funcTot += func1D(fRandX);
+  }
+
+  funcMean = funcTot/nRand;
+
+  return funcMean;
+}
---
+++ integrateMean1D.h
@@ -0,0 +1,3 @@
+// integrateMean1D.h
+
+int integrate1D(int nRand, float lowerLimitX, float upperLimitX, float (*func1D)(float));
