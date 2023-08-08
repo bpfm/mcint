b50466097088b48ec3ff86f40604bee2ce764726
Ben Morton <morton@roe.ac.uk>
2023-08-08 17:00:40 +0100

    continued removing debug

--- integrateMean2DSPAlpha.h
+++ integrateMean2DSPAlpha.h
@@ -1,3 +1,3 @@
 // integrateMean2DSPAlpha.h
 
-float integrateMean2DSPAlpha(float sp, float Rp, float zp, long int nRand, float lowerLimitX, float upperLimitX, float lowerLimitY, float upperLimitY, FILE *testFile);
+float integrateMean2DSPAlpha(float sp, float Rp, float zp, long int nRand, float lowerLimitX, float upperLimitX, float lowerLimitY, float upperLimitY);
