975880eb1b95a56776b5fe4feca4b5feaa6ecc0d
Ben Morton <morton@roe.ac.uk>
2025-02-16 22:55:26 +0000

    minor changes for extended perturber

--- integrateMean2DSPAlpha.cpp
+++ integrateMean2DSPAlpha.cpp
@@ -19,6 +19,21 @@ float integrateMean2DSPAlpha(float sp, float Rp, float zp, long int nRand, float
   for(int i=0; i<nRand; i++){
     fRandX   = lowerLimitX   + static_cast <float> (rand()) / (static_cast <float> (float(RAND_MAX)/(upperLimitX   - lowerLimitX)));
     fRandY   = lowerLimitY   + static_cast <float> (rand()) / (static_cast <float> (float(RAND_MAX)/(upperLimitY   - lowerLimitY)));
+
+    // if(fRandX < lowerLimitX*(1.0+1.0E-4)){   // avoid singularities
+    //   fRandX = lowerLimitX*(1.0+1.0E-4);
+    // }
+    // if(fRandX > upperLimitX*(1.0-1.0E-4)){
+    //   fRandX = upperLimitX*(1.0-1.0E-4);
+    // }
+
+    // if(fRandY < lowerLimitY*(1.0+1.0E-4)){
+    //   fRandY = lowerLimitY*(1.0+1.0E-4);
+    // }
+    // if(fRandY > upperLimitY*(1.0-1.0E-4)){
+    //   fRandY = upperLimitY*(1.0-1.0E-4);
+    // }
+
     // funcTot += alphaExtended(rp,phip,1,fRandX,fRandY,testFile);
     funcTot += alphaExtended(sp,Rp,1,fRandX,fRandY);
   }
